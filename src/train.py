b5a910ff45722e68bf736a2b56732a6345802e4a
kazuki.masumura <kazuki.masumura@adminnoMacBook-puro-8.local>
2018-09-07 18:09:16 +0900

    change before connect time

--- src/train.py
+++ src/train.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
 
         # サーバの起動
         cmd = \
-            "rcssserver server::half_time = -1 server::send_step = 3 server::sense_body_step = 2 server::simulator_step = 2 server::auto_mode = true server::kick_off_wait = 60000"
+            "rcssserver server::send_step = 3 server::sense_body_step = 2 server::simulator_step = 2 server::auto_mode = true server::connect_wait = 600"
         server = subprocess.Popen(cmd.split())
 
         # モニタの起動
